7f630ba071f825155605418a628113923ede45da
Steve Sulev <steve.sulev@itcollege.ee>
2015-10-13 23:28:07 +0300

    Harjutus 3.1 tehtud

--- src/teema1/Harjutus3_Juhuslikkus.java
+++ src/teema1/Harjutus3_Juhuslikkus.java
@@ -27,6 +27,8 @@ import java.util.Scanner;
 public class Harjutus3_Juhuslikkus {
     public static void main(String[] args) {
         kullJaKiri();
+        liist();
+        taring();
     }
 
     public static void kullJaKiri() {
@@ -42,7 +44,7 @@ public class Harjutus3_Juhuslikkus {
         while (true) {
             String mynt = sisestus.nextLine();
             //System.out.println(mynt != "kull");
-            if (!mynt.equals("kull") && !mynt.equals("kiri")){
+            if (!mynt.equals("kull") && !mynt.equals("kiri")) {
                 System.out.println("Õpi sisestama! Sobib ainult kull või kiri!");
                 sisestus = new Scanner(System.in);
             } else {
@@ -56,6 +58,38 @@ public class Harjutus3_Juhuslikkus {
             }
         }
     }
+
+    public static void liist() {
+        Scanner sisestus = new Scanner(System.in);
+        System.out.println("Mitu mängijat?");
+        int arv = sisestus.nextInt();
+        System.out.println("Mängijaid on kokku " + arv + "tükki");
+        int valik = new Random().nextInt(arv) + 1;
+        System.out.println("Valituks osutus mängija " + valik);
+    }
+
+    public static int veere () {
+        return new Random().nextInt(6) + 1;
+    }
+    public static void taring() {
+
+        int mangijaSkoor = veere() + veere();
+        int arvutiSkoor = veere() + veere();
+
+        System.out.println("Mängija sai kokku " + mangijaSkoor + "punkti.");
+        System.out.println("Arvuti sai kokku " + arvutiSkoor + "punkti.");
+
+        if (mangijaSkoor > arvutiSkoor) {
+            System.out.println("Võitsid!");
+        } else if (arvutiSkoor < mangijaSkoor) {
+            System.out.println("Kaotasid!");
+        } else {
+            System.out.println("Viik!");
+        }
+    }
 }
+
+
+
 
 
--- src/teema1/Harjutus4_massiivid.java
+++ src/teema1/Harjutus4_massiivid.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import java.util.*;
+
 /**
  * 1. Salvesta kasutajalt 10 numbrit ja sorteeri suuruse järgi. (int[])
  * 2. Salvesta kasutajalt 10 sõne ja sorteeri tähestikulises järjekorras. (String[])
@@ -9,4 +11,32 @@ package teema1;
  * main() meetodi saad lühendiga psvm.
  */
 public class Harjutus4_massiivid {
+    public static void main (String[] args){
+        numbrid(10);
+    }
+    public static void numbrid (int nr) {
+        Scanner sisestus = new Scanner(System.in);
+        int arvud [] = new int [nr];
+        int reverse [] = new int [nr];
+
+        for (int i = 0; i < nr; i++) {
+            System.out.println("Sisesta arv");
+            int arv = sisestus.nextInt();
+            arvud[i]=arv;
+        }
+        System.out.println("Sisestatud arvud:");
+        System.out.println(Arrays.toString(arvud));
+
+        Arrays.sort(arvud);
+        System.out.println("Sisestatud arvud järjestades väikseimast suurimani:");
+        System.out.println(Arrays.toString(arvud));
+
+        // Miks järgnev ei toimi?
+       for (int j = nr; j > 0; j--) {
+            int l = nr - j;
+            reverse[j] = arvud[l];
+        }
+        System.out.println(Arrays.toString(reverse));
+    }
 }
+
